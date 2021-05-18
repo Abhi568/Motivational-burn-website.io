d744c39569a89231e8f705f3cac01fdc2a62cf17
Abhishek Jaiswal <62826509+Abhi568@users.noreply.github.com>
2021-05-18 10:50:44 +0530

    update name_remain.php
    update name_remain.php

--- name_remain.php
+++ name_remain.php
@@ -3,14 +3,14 @@ session_start();
     if (isset($_SESSION['uid']))
     {
         ?>
-        <script>
-        var output = '<?php echo $_SESSION['name']; ?>';
-        var image = '<?php echo '../upload/'.$_SESSION['image']; ?>';
-        document.getElementById("name").innerHTML=output;
-        console.log(image);
-        document.getElementById("images").src=image;
-        console.log(image);
-        </script>
+            <script>
+                var output = '<?php echo $_SESSION['name']; ?>';
+                var image = '<?php echo '../upload/'.$_SESSION['image']; ?>';
+                document.getElementById("name").innerHTML=output;
+                console.log(image);
+                document.getElementById("images").src=image;
+                console.log(image);
+            </script>
         <?php
     }
-    ?>
+?>
