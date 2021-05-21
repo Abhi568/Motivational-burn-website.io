0234e6bb87d89214c386150b34cd4dd3cfd99305
Abhishek Jaiswal <62826509+Abhi568@users.noreply.github.com>
2021-05-21 17:52:30 +0530

    connection.php update
    connection.php update

--- connection.php
+++ connection.php
@@ -3,7 +3,6 @@ $Server="localhost";
 $username="root";
 $password="";
 $database_table="login_details";
-
 $connection=mysqli_connect($Server,$username,$password,$database_table);
 if ($connection)
 {
