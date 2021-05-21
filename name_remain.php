a01b4fc8c8002d03ef919be90a589bf3bf9c7b77
Abhishek Jaiswal <62826509+Abhi568@users.noreply.github.com>
2021-05-21 17:50:48 +0530

    Add files via upload
    name_remain.php update

--- name_remain.php
+++ name_remain.php
@@ -1,16 +1,15 @@
 <?php
-session_start();
-if (isset($_SESSION['uid']))
-{
-    ?>
+    if (isset($_SESSION['uid']))
+    {
+        ?>
         <script>
-            var output = '<?php echo $_SESSION['name']; ?>';
-            var image = '<?php echo '../upload/'.$_SESSION['image']; ?>';
-            document.getElementById("name").innerHTML=output;
-            console.log(image);
-            document.getElementById("images").src=image;
-            console.log(image);
+        var output = '<?php echo $_SESSION['name']; ?>';
+        var image = '<?php echo '../upload/'.$_SESSION['image']; ?>';
+        document.getElementById("name").innerHTML=output;
+        console.log(image);
+        document.getElementById("images").src=image;
+        console.log(image);
         </script>
-    <?php
-}
-?>
+        <?php
+    }
+    ?>
